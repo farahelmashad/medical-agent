761651e5d733ff5c1055030846a4580ae6bce9b0
Farah Hosam Sayed Ahmed ElMashad <144489556+farahelmashad@users.noreply.github.com>
2026-04-15 17:17:22 +0200

    update setup
    Removed print statements and added a comment for future removal.

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import json
 import sys
 import os
-
-# Make sure Python can find the app package
+#trial file , will remove later 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from app.sheets import generate_slots
 
 with open("data/doctors.json", "r", encoding="utf-8") as f:
     data = json.load(f)
-
-print("Generating slots for all doctors...")
+    
 generate_slots(data["doctors"], days_ahead=14)
-print("Done! Check your Google Sheet slots tab.")
